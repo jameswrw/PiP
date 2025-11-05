691a8a30e6aba18e98a4f2a7a582665428d569c5
James Weatherley <james@weatherley.net>
2025-11-05 13:33:25 +0000

    Initial Commit

---
+++ PiP/PiPApp.swift
@@ -0,0 +1,17 @@
+//
+//  PiPApp.swift
+//  PiP
+//
+//  Created by James Weatherley on 05/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct PiPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PiPTests/PiPTests.swift
@@ -0,0 +1,17 @@
+//
+//  PiPTests.swift
+//  PiPTests
+//
+//  Created by James Weatherley on 05/11/2025.
+//
+
+import Testing
+@testable import PiP
+
+struct PiPTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
