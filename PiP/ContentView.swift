05aebbfeeaa3a190ae02fc5f0e1353009498f3da
James Weatherley <james@weatherley.net>
2025-11-05 15:17:03 +0000

    Initial commit

--- PiP/ContentView.swift
+++ PiP/ContentView.swift
@@ -6,16 +6,33 @@
 //
 
 import SwiftUI
+import WebKit
+
+struct MyVideoWebView: UIViewRepresentable {
+    func makeUIView(context: Context) ->  WKWebView {
+        let webView = WKWebView()
+        webView.configuration.allowsInlineMediaPlayback = true
+        webView.configuration.allowsPictureInPictureMediaPlayback = true
+        webView.configuration.mediaTypesRequiringUserActionForPlayback = []
+
+        return webView
+    }
+    
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        uiView.scrollView.isScrollEnabled = false
+        
+        let path = Bundle.main.path(forResource: "Critter", ofType:"mp4")!
+        let url = URL(fileURLWithPath: path)
+        uiView.load(URLRequest(url: url))
+    }
+}
 
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            MyVideoWebView()
+                .frame(height: 300)
         }
-        .padding()
     }
 }
 
